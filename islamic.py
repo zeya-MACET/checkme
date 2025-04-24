a4e0e1134ff8a225c76660f7a50df7c1d295d3ad
zeya-MACET <zeyaulislamp@gmail.com>
2025-04-24 09:46:43 +0300

    Add files via upload

--- islamic.py
+++ islamic.py
@@ -1,60 +1,26 @@
 import streamlit as st
 
 def app():
-    #Title
-    #st.title("welcome to My :violet[Trending] Page Website in Python")
-    #Header
+    
     st.header(":violet[My Islamic page]")
 
-    #st.button("Website Development is in Progress")
-    #st.image(E:\My Development\Python\Website in Python\solar.png)
-    #subheader
-    #st.subheader("Streamlit Tutorial")
-    #text
-    #st.text(''' Helloe Everyone Welcome to my Python Station Streamlit 
-            #Hope you will check it......''')
-    #write
-    #st.write('Please Subscribe my Python Channel')
-    #Image and Caption
+    
     from PIL import Image
     img = Image.open('Islamic.png')
     st.image(img,caption='Islamic Holy Places')
-    #Colourful text
-    #success
-    st.text("Makkah Information")
-    makkah=("https://en.wikipedia.org/wiki/Mecca")
-    st.write(makkah)
-
-    st.text("Makkah Masjeda Al Haram Live 1 to 3 ")
-    makkahlive1=("https://www.youtube.com/watch?v=eJNWHKld5E8")
-    makkahlive2=("https://www.youtube.com/watch?v=PPVvoWbReMI")
-    makkahlive3=("https://www.youtube.com/watch?v=bNY8a2BB5Gc")
-    st.write(makkahlive1)
-    st.write(makkahlive2)
-    st.write(makkahlive3)
-    st.text("Madinah Information")
-    madinah=("https://en.wikipedia.org/wiki/Medina")
-    st.write(madinah)
-    st.text("Madinah Masjeda Al Nabwi Live 1 to 3 ")
-    madinahlive1=("https://www.youtube.com/watch?v=rHWSRMcGGBQ")
-    madinahlive2=("https://www.youtube.com/watch?v=yWw0DWJQVtU")
-    madinahlive3=("https://www.youtube.com/watch?v=naaOMgZbIHQ")
-    st.write(madinahlive1)
-    st.write(madinahlive2)
-    st.write(madinahlive3)
-
-    st.text("Masjeda Al Nabwi Madinah Information")
-    madinahm=("https://simple.wikipedia.org/wiki/Al-Masjid_an-Nabawi")
-    st.write(madinahm)
-
-    st.text("Masjeda Al Haram Makkah Information")
-    makkahm=("https://en.wikipedia.org/wiki/Masjid_al-Haram")
-    st.write(makkahm)
-
-    st.text("Masjeda Al Aksa Information")
-    aksa=("https://en.wikipedia.org/wiki/Al-Aqsa_Mosque")
-    st.write(aksa)
-
+  
+    st.write("[Makkah](https://en.wikipedia.org/wiki/Mecca)")
+    st.write("[Madinah](https://en.wikipedia.org/wiki/Medina)")
+    st.write("[Masjeda Al Nabwi Madinah ](https://simple.wikipedia.org/wiki/Al-Masjid_an-Nabawi)")
+    st.write("[Masjid al-Aqsa (Bayt al-Maqdis) Jerusalem](https://en.wikipedia.org/wiki/Al-Aqsa_Mosque)")
+    st.write("[Masjeda Al Haram Makkah](https://en.wikipedia.org/wiki/Masjid_al-Haram)")    
+    st.write("[Makkah Masjeda Al Haram Live 1](https://www.youtube.com/watch?v=eJNWHKld5E8)")
+    st.write("[Makkah Masjeda Al Haram Live 2](https://www.youtube.com/watch?v=PPVvoWbReMI)")
+    st.write("[Makkah Masjeda Al Haram Live 3](https://www.youtube.com/watch?v=bNY8a2BB5Gc)")
+    st.write("[Madinah Masjeda Al Nabwi Live 1](https://www.youtube.com/watch?v=rHWSRMcGGBQ)")
+    st.write("[Madinah Masjeda Al Nabwi Live 2](https://www.youtube.com/watch?v=yWw0DWJQVtU)")
+    st.write("[Madinah Masjeda Al Nabwi Live 3](https://www.youtube.com/watch?v=naaOMgZbIHQ)")
+    
 
     st.success('success')
     
--- watch.py
+++ watch.py
@@ -2,18 +2,7 @@ import streamlit as st
 import pandas as pd
 import streamlit as st
 def app():
-    #Title
-    #st.title("welcome to My :blue[ACCOUNT] Page Website in Python")
-    #Header
-    #st.header(":blue[My General Information Page]")
-
-    #st.button("Website Development is in Progress")
-    #st.image(E:\My Development\Python\Website in Python\solar.png)
-    #subheader
     
-    #import streamlit as st
-
-    #st.title("Google Search Bar in Streamlit")
     st.header(":blue[My General Information]")
     # Embed Google Search Bar
     google_search_html = """    
@@ -28,34 +17,24 @@ def app():
     # Display HTML using Streamlit's unsafe_html
     st.markdown(google_search_html, unsafe_allow_html=True)
 
-    #text
-    #st.text(''' Helloe Everyone Welcome to my Python Station Streamlit 
-            #Hope you will check it......''')
-    #bbchindinews=("BBC HINDI https://www.bbc.com/hindi")
-
-    #st.write(bbchindinews)
-    #############################################################################################
-    # Create a dropdown list
-    #selected_option = st.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
-
-    # Display the selected option
-    #st.write(f"You selected: {selected_option}")
+   
     ##############################################################################################
     st.subheader(':green[My News Collection]')
 
     ############$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     # Create a sample dataframe
+
     data = {
     'Category': ['Arabic ', 'English ', 'Arabic ', 'Hindi ', 'Urdu ',
                  'English ', 'Urdu ', 'Hindi ', 'Arabic ', 'Hindi ', 'English '
                  , 'English ', 'English ', 'English ', 'English ', 'Urdu ', 'Urdu ', 'Urdu ', 'Arabic ', 'Arabic ', 'Arabic ', 'English ', 'Hindi ', 'Hindi '],
-    'Subcategory': ['Alwatan https://www.alwatan.com.sa/', 'Saudigazette https://pdf.saudigazette.com.sa/', 'Okaz https://www.okaz.com.sa/', 
-                    'Dainak Jagran https://www.jagran.com/', 'Siasat https://epaper.siasat.com/','Arabnews https://www.arabnews.com/saudiarabia/', 
-                    'Inquilab https://www.inquilab.com/', 'NDTV https://ndtv.in/', 'Makkah https://makkahnewspaper.com/', 'BBC https://www.bbc.com/hindi/', 'The Indian Express https://indianexpress.com/'
-                    , 'CNN https://edition.cnn.com/', 'Times of India https://timesofindia.indiatimes.com//', 'Hindustan Times https://www.hindustantimes.com/', 'Alzagira https://www.aljazeera.com/'
-                    , 'Qaumitanzeem https://qaumitanzeem.com/', 'Qaumiawaz https://www.qaumiawaz.com/', 'Taasir https://epaper.taasir.com/page.php'
-                    , 'Aljazeera https://www.aljazeera.net/', 'Aawsat https://aawsat.com/', 'BBC https://www.bbc.com/arabic', 'BBC https://www.bbc.com/', 'Dainak Bhaskar https://www.bhaskar.com/'
-                    , 'Navbharattimes https://navbharattimes.indiatimes.com/headlines.cms']
+    'Subcategory': ["[Alwatan](https://www.alwatan.com.sa/)", '[Saudigazette](https://pdf.saudigazette.com.sa/)', '[Okaz](https://www.okaz.com.sa/)', 
+                    '[Dainak Jagran](https://www.jagran.com/)', '[Siasat](https://epaper.siasat.com/)','[Arabnews](https://www.arabnews.com/saudiarabia/)', 
+                    '[Inquilab](https://www.inquilab.com/)', '[NDTV](https://ndtv.in/)', '[Makkah](https://makkahnewspaper.com/)', '[BBC](https://www.bbc.com/hindi/)', '[The Indian Express](https://indianexpress.com/)'
+                    , '[CNN](https://edition.cnn.com/)', '[Times of India](https://timesofindia.indiatimes.com/)', '[Hindustan Times](https://www.hindustantimes.com/)', '[Alzagira](https://www.aljazeera.com/)'
+                    , '[Qaumitanzeem](https://qaumitanzeem.com/)', '[Qaumiawaz](https://www.qaumiawaz.com/)', '[Taasir](https://epaper.taasir.com/page.php)'
+                    , '[Aljazeera](https://www.aljazeera.net/)', '[Aawsat](https://aawsat.com/)', '[BBC](https://www.bbc.com/arabic)', '[BBC](https://www.bbc.com/)', '[Dainak Bhaskar](https://www.bhaskar.com/)'
+                    , '[Navbharattimes](https://navbharattimes.indiatimes.com/headlines.cms)']
     }
     df = pd.DataFrame(data)
 
@@ -66,14 +45,16 @@ def app():
     filtered_df = df[df['Category'] == selected_category]
 
     # Create the dependent dropdown for Subcategory
+    #selected_subcategory = st.selectbox("Select  Newswebsite", st.write(filtered_df['Subcategory'].unique()))
+
+    #st.write(f":green[Select  Newswebsite:]", filtered_df['Subcategory'].unique())
+
     selected_subcategory = st.selectbox("Select  Newswebsite", filtered_df['Subcategory'].unique())
 
     # Display the selected options
     st.write(f":green[You selected:] :red[{selected_category}] - {selected_subcategory}")
     ############$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
-    #write
-    #st.write('Please Subscribe my Python Channel')
     #Image and Caption
     from PIL import Image
     img = Image.open('enews.png')
